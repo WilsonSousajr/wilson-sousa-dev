23c25f42afd86f1c612f7a47cb169444c1ac1d61
Wilson Sousa <wstechnology.br@gmail.com>
2024-07-09 20:09:09 -0300

    ✨ Add: Import and use Header component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header'
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const navItems = [
+    { label: 'Home', href: '/' },
+    { label: 'Projects', href: '#projects' },
+    { label: 'Experience', href: '#experience' },
+    { label: 'Contact', href: '#contact' },
+
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header logo="WS" navItems={navItems}/>
     </div>
   );
 }
