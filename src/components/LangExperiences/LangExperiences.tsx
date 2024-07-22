6b8e17e05129dd4b1a91f8a63753e49c28cb6e69
Wilson Sousa <wstechnology.br@gmail.com>
2024-07-22 17:58:09 -0300

    🚧 (WIP): Create LangExperience component

---
+++ src/components/LangExperiences/LangExperiences.tsx
@@ -0,0 +1,13 @@
+import LangExperiencesList from "./LangExperiencesList";
+
+const LangExperiences = () => {
+
+    return (
+        <section>
+            <h3 id="lang-experiences-title" className="title-h3">Experience With</h3>
+            <LangExperiencesList />
+        </section>
+    )
+}
+
+export default LangExperiences;
